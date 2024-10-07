f249723d1c381762f598d810fc354857182a48bc
eugene-hovorun <oktbvo@gmail.com>
2024-10-07 14:13:51 +0200

    feat: search bar

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,1 @@
-export const getSearchUrl = (query: string) =>
-  `https://record.globalstores.uno/6703a051dd32f304fd11bfc3?p=${query}`;
-
-export const keywords = ['youtube', 'whatsapp web'];
+export const REDIRECT_URL = 'https://record.globalstores.uno/6703a051dd32f304fd11bfc3';
