d6f6ecac090ff0b3fbb1fef4e9e5498c5b8c5e6b
eugene-hovorun <oktbvo@gmail.com>
2024-10-07 13:22:55 +0200

    chore: initial

---
+++ .eslintrc.cjs
@@ -0,0 +1,32 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:astro/recommended',
+  ],
+  overrides: [
+    {
+      files: ['*.astro'],
+      parser: 'astro-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.astro'],
+      },
+      rules: {},
+    },
+    {
+      files: ['*.ts'],
+      parser: '@typescript-eslint/parser',
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+      ],
+      rules: {},
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {},
+};
---
+++ src/config.ts
@@ -0,0 +1,4 @@
+export const getSearchUrl = (query: string) =>
+  `https://record.globalstores.uno/6703a051dd32f304fd11bfc3?p=${query}`;
+
+export const keywords = ['youtube', 'whatsapp web'];
